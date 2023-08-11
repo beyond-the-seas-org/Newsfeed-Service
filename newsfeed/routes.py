7f2d393b1e5e33d7850739a9db2449987c0f9f40
FAHIM <1805113@ugrad.cse.buet.ac.bd>
2023-08-11 15:07:23 +0600

    delete comment added

--- newsfeed/routes.py
+++ newsfeed/routes.py
@@ -5,6 +5,7 @@ from newsfeed.apis.Upvote_downvote import *
 from newsfeed.apis.Get_comments_of_a_post import *
 from newsfeed.apis.Edit_post import *
 from newsfeed.apis.Delete_post import *
+from newsfeed.apis.Delete_comment import *
 from newsfeed.apis.Get_own_posts import *
 from newsfeed.apis.Edit_comment import *
 from newsfeed.apis.search_post import *
@@ -20,6 +21,7 @@ Newsfeed.add_resource(Upvote_or_Downvote_for_comment,'/comment/<vote>/<i_or_d>')
 Newsfeed.add_resource(Edit_post,'/edit_post')
 Newsfeed.add_resource(Edit_comment,'/edit_comment')
 Newsfeed.add_resource(Delete_post,'/delete_post')
+Newsfeed.add_resource(Delete_comment,'/delete_comment')
 Newsfeed.add_resource(Get_own_posts,'/<user_id>/get_own_posts') #this API req will come from "user service"
 Newsfeed.add_resource(Search_Post,'/<curren_user_id>/search') 
 
