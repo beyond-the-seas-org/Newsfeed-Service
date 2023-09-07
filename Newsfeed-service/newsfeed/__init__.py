51718194c1044f757c4878ef220e591b9fcd68ab
asifhaider <1805112@ugrad.cse.buet.ac.bd>
2023-09-08 03:34:14 +0600

    get all post api protected, newsfeed service jwt registered

--- Newsfeed-service/newsfeed/__init__.py
+++ Newsfeed-service/newsfeed/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import psycopg2, dotenv, os
 from flask_restx import Api
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,9 +15,12 @@ db_url = os.getenv('DATABASE_URL')
 
 conn = psycopg2.connect(db_url, sslmode='prefer')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config.from_object('newsfeed.config.DevelopmentConfig')
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 from newsfeed.routes import *
